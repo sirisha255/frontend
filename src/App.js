ab092029940a447129ac14f3d2c0814c67323955
sirisha255 <112860662+sirisha255@users.noreply.github.com>
2023-02-08 11:04:08 +0100

    working...

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import Data from "./Data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a href="/">Emart</a>
-      </header>
+    <div>
+      <header> <a href="/">EBUTIK</a> </header>
       <main>
-        List of Products
-      </main>
-    </div>
+        <h1>Products</h1>
+
+        <div className="products">
+        
+         { 
+         Data.products.map((product) =>(
+            <div className="product" key={product.slug}>
+           <a href={'/product/$ {product.slug}'}>           
+            <img src={product.image} alt={product.name} />
+          </a>
+          <div className="product-info">
+          <a href={'/product/$ {product.slug}'}>           
+
+               <p>{product.name}</p>
+               </a>
+               <p><strong>${product.price}</strong></p>
+           <button>Add to cart</button>
+         
+         
+          </div>
+          </div>
+         ))
+         }
+  
+        </div>
+    </main>
+   </div>
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,39 @@
+const Data = {
+    products : [
+        {
+           name:'Nike',
+           category:'shirts',
+           image:'./images/ms_n0kdq_512_89696088.jpg',
+           
+           price:120,
+           countinstock:10,
+           brand:'nike',
+           rating:4.5,
+           numReviews:10,
+           description:'high quality',
+        },
+        {
+            name:'puma',
+            category:'shirts',
+            image:'images/ms_rkfhe_512_72007795.jpg',
+            price:120,
+            countinstock:10,
+            brand:'puma',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality',
+         },
+         {
+            name:'Adidas',
+            category:'shirts',
+            image:'images/ms_earjc_512_35462516.jpg',
+            price:120,
+            countinstock:10,
+            brand:'adidas',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality',
+         }     
+        ],
+};
+export default Data;
